22832bb61197b40fd22e655c89a2d9d4c5e73d4f
Will Craftia <willcraftia@gmail.com>
2012-07-19 23:32:36 +0900

    added.

---
+++ NoiseMeasuring/FloorMeasuring/Program.cs
@@ -0,0 +1,54 @@
+﻿#region Using
+
+using System;
+using System.Diagnostics;
+
+#endregion
+
+namespace FloorMeasuring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            int loopCount = 100000;
+
+            Console.WriteLine("Loop = {0}", loopCount);
+
+            Console.WriteLine("[Math.Floor]", loopCount);
+
+            stopwatch.Start();
+            for (int i = 0; i < loopCount; i++)
+            {
+                var v = (int) Math.Floor(0d);
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine("Elapsed = {0}", stopwatch.Elapsed);
+
+            stopwatch.Reset();
+
+            Console.WriteLine("[Custom Floor]", loopCount);
+
+            stopwatch.Start();
+            for (int i = 0; i < loopCount; i++)
+            {
+                var v = CustomFloor(0d);
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine("Elapsed = {0}", stopwatch.Elapsed);
+
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+
+        static int CustomFloor(double v)
+        {
+            return 0 < v ? (int) v : (int) v - 1;
+        }
+    }
+}
