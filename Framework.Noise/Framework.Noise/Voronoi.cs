078749bb38044c7b73029246a13fc155ae298bc5
Will Craftia <willcraftia@gmail.com>
2012-07-23 16:04:59 +0900

    mistake.

--- Framework.Noise/Framework.Noise/Voronoi.cs
+++ Framework.Noise/Framework.Noise/Voronoi.cs
@@ -23,7 +23,7 @@ namespace Willcraftia.Framework.Noise
 
         VoronoiType voronoiType = VoronoiType.First;
 
-        MetricsDelegate metrics = Metrics.Squared;
+        MetricsDelegate metrics = Willcraftia.Framework.Noise.Metrics.Squared;
 
         public int Seed
         {
